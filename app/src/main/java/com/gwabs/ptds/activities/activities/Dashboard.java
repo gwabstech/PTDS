443cea09ef7a2c28cc02e1007da04368b15f3e13
Abubakar Abdullahi Gwabare <gwabstech@gmail.com>
2022-06-22 19:04:57 +0100

    setup nav components

--- app/src/main/java/com/gwabs/ptds/activities/activities/Dashboard.java
+++ app/src/main/java/com/gwabs/ptds/activities/activities/Dashboard.java
@@ -1,42 +1,59 @@
 package com.gwabs.ptds.activities.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.navigation.NavigationView;
 import com.gwabs.ptds.R;
 
 public class Dashboard extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private NavController navController;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboad);
-
-        /*
-
-        NavHostFragment navHostFragment = (NavHostFragment) this.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        assert navHostFragment != null;
-        NavController navController = navHostFragment.getNavController();
-        NavigationView navView = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawerLayout);
+         navigationView = findViewById(R.id.nav_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        NavigationUI.setupWithNavController(navView, navController);
-        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
-        setSupportActionBar(toolbar);
 
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         assert navHostFragment != null;
+        navController = navHostFragment.getNavController();
         AppBarConfiguration appBarConfiguration =
                 new AppBarConfiguration.Builder(navController.getGraph())
-                        .setDrawerLayout(drawerLayout)
+                        .setOpenableLayout(drawerLayout)
                         .build();
 
-        NavigationUI.setupWithNavController(
-                toolbar, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(toolbar,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView,navController);
 
 
 
-         */
+
+
+
+        
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+
+        return NavigationUI.onNavDestinationSelected(item,navController) || super.onOptionsItemSelected(item);
     }
 
 
